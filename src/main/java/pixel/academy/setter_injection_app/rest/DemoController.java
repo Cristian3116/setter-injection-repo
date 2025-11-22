c7f1a5b00c636ed4c903cf9d9efdfcde1151ff2d
CristianM <mija.cristian3101@gmail.com>
2025-11-22 13:09:16 +0200

    Inital commit

--- src/main/java/pixel/academy/setter_injection_app/rest/DemoController.java
+++ src/main/java/pixel/academy/setter_injection_app/rest/DemoController.java
@@ -1,6 +1,7 @@
 package pixel.academy.setter_injection_app.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pixel.academy.setter_injection_app.common.Chef;
@@ -12,7 +13,7 @@ public class DemoController {
     private Chef myChef;
 
     @Autowired
-    public DemoController(Chef theChef) {
+    public DemoController(@Qualifier("turkishChef") Chef theChef) {
         myChef = theChef;
     }
 
